340321394828de1346732620ac4fcf7e9eee0d10
Murbarkh <murbarkh.hamid@gmail.com>
2023-11-29 19:24:40 +0000

    Add display in home.html

--- things/forms.py
+++ things/forms.py
@@ -1,11 +1,17 @@
 """Forms of the project."""
 from django import forms
 from .models import Thing
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class ThingForm(forms.Form):
     model = Thing
 
-    fields = ['name', 'description', 'quantity']
-    widgets = {'description' : forms.Textarea(),
-               'quantity' : forms.NumberInput()}
+    name = forms.CharField(max_length=35)
+    description = forms.CharField(max_length=120, widget=forms.Textarea())
+    quantity = forms.IntegerField(
+        validators=[MinValueValidator(0),MaxValueValidator(50)],
+        widget=forms.NumberInput()
+    )
+
+
